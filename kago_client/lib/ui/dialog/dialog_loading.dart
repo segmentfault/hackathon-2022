b09db1739f8326aa52d6ac7113c9d76d0f05ca2f
liaoyp <251180323@qq.com>
2022-09-01 01:46:27 +0800

    优化加载动画

--- kago_client/lib/ui/dialog/dialog_loading.dart
+++ kago_client/lib/ui/dialog/dialog_loading.dart
@@ -20,35 +20,39 @@ class LoadingDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material( //创建透明层
-      type: MaterialType.transparency, //透明类型
-      child: Center( //保证控件居中效果
-        child: SizedBox(
-          width: 100,
-          height: 100,
-          child: Container(
-            decoration: const ShapeDecoration(
-              color: Colors.black45,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(8.0),
+    return Material(
+        //创建透明层
+        type: MaterialType.transparency, //透明类型
+        child: Center(
+          //保证控件居中效果
+          child: SizedBox(
+            width: 100,
+            height: 100,
+            child: Container(
+              decoration: const ShapeDecoration(
+                color: Colors.black45,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8.0),
+                  ),
                 ),
               ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Center(
-                  child: Lottie.asset(R.assetsLottieLoading,
-                      width: 60, animate: true),
-                ),
-                Text(text, style: Styles.style_white_14)
-              ],
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  // Center(
+                  //   child: Lottie.asset(R.assetsLottieLoading,
+                  //       width: 60, animate: true),
+                  // ),
+                  Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  Text(text, style: Styles.style_white_14)
+                ],
+              ),
             ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
