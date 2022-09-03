8a18c11c0f7147fefc0f7c72162b209845f005e3
liaoyp <251180323@qq.com>
2022-09-03 21:57:25 +0800

    代码优化

--- kago_client/lib/ui/page/chat_page/chat_page.dart
+++ kago_client/lib/ui/page/chat_page/chat_page.dart
@@ -32,7 +32,7 @@ class ChatPage extends GetSaveView<ChatController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("考研打卡群"),
+        title: const Text("考研打卡群"),
       ),
       body: Stack(
         children: [
@@ -42,8 +42,7 @@ class ChatPage extends GetSaveView<ChatController> {
             height: Get.height,
           ),
           ListView.builder(
-              padding:
-                  EdgeInsets.only(left: 12, top: 12, right: 12, bottom: 144),
+              padding: const EdgeInsets.only(left: 12, top: 12, right: 12, bottom: 144),
               itemCount: controller.chatMessageList.length,
               itemBuilder: (BuildContext context, int index) {
                 ChatMessage item = controller.chatMessageList[index];
@@ -69,14 +68,14 @@ class ChatPage extends GetSaveView<ChatController> {
                   controller.addSendMessage(text);
                 }
               },
-              actions: [
+              actions: const [
                 InkWell(
                   child: Icon(
                     Icons.add,
                     color: Colors.black,
                     size: 24,
                   ),
-                  onTap: () {},
+                  // onTap: () {},
                 ),
                 Padding(
                   padding: EdgeInsets.only(left: 8, right: 8),
@@ -86,7 +85,7 @@ class ChatPage extends GetSaveView<ChatController> {
                       color: Colors.green,
                       size: 24,
                     ),
-                    onTap: () {},
+                    // onTap: () {},
                   ),
                 ),
               ],
@@ -102,8 +101,7 @@ class ChatPage extends GetSaveView<ChatController> {
 class SigninItem extends StatelessWidget {
   final bool isSender;
   final String text;
-  const SigninItem({Key? key, required this.isSender, required this.text})
-      : super(key: key);
+  const SigninItem({Key? key, required this.isSender, required this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -119,17 +117,17 @@ class SigninItem extends StatelessWidget {
           width: 15,
         ),
         Container(
-          padding: EdgeInsets.all(12),
-          margin: EdgeInsets.symmetric(vertical: 10),
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(vertical: 10),
           decoration: DecorationStyle.customizeBorder(
-                  radius: 15, color: Color.fromRGBO(0, 153, 255, 1))
-              .copyWith(color: Color.fromRGBO(0, 153, 255, 1)),
+                  radius: 15, color: const Color.fromRGBO(0, 153, 255, 1))
+              .copyWith(color: const Color.fromRGBO(0, 153, 255, 1)),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
-                children: [
+                children: const [
                   Icon(
                     Icons.check_box,
                     color: Colors.white,
@@ -137,10 +135,8 @@ class SigninItem extends StatelessWidget {
                   ),
                   Text(
                     "沈公子 完成打卡！",
-                    style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold),
+                    style:
+                        TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.bold),
                   ),
                 ],
               ),
@@ -164,8 +160,7 @@ class SigninItem extends StatelessWidget {
 class ChatItem extends StatelessWidget {
   final bool isSender;
   final ChatMessage item;
-  const ChatItem({Key? key, required this.isSender, required this.item})
-      : super(key: key);
+  const ChatItem({Key? key, required this.isSender, required this.item}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
